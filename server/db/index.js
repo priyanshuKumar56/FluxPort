e0dfbc31a967289b731422691327a0673cfdab6f
priyanshuKumar56 <109659772+priyanshuKumar56@users.noreply.github.com>
2026-01-09 22:50:11 +0530

    osme

--- server/db/index.js
+++ server/db/index.js
@@ -5,13 +5,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-<<<<<<< Updated upstream
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || 'fluxport',
-  user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || 'Adminpsql@123',
-=======
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
   host: process.env.DB_HOST ,
@@ -19,8 +12,6 @@ const pool = new Pool({
   database: process.env.DB_NAME ,
   user: process.env.DB_USER ,
   password: process.env.DB_PASSWORD ,
-
->>>>>>> Stashed changes
 });
 
 export default pool;
